5aff0fe64c5925962a138cc93ddf66eaff4a8fb9
Elias Mimouni <emimouni10@hotmail.com>
2016-10-28 06:28:23 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,37 @@
 package com.company;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	int grades [] = new int[1000];
-    Scanner input = new Scanner(System.in);
-        int grade;
-        int count = 0;
+        Scanner kb = new Scanner(System.in);
+        String[] names = new String[1000];
+        int[] grades = new int[1000];
+        boolean b = true;
+        int counter = 0;
+        while (b==true){
+            System.out.println("Enter a name.");
+            String input = kb.next();
+            names[counter] = input;
+            System.out.println("Enter a number");
+            int num = kb.nextInt();
+            grades[counter] = num;
+            System.out.println("Would you like to continue?");
+            String response = kb.next();
+            response = response.toUpperCase();
+            if (response.equals("NO")){
+                b = false;
+            }
+            counter++;
+        }
+        for (int i = 0; i<counter; i++){
+            System.out.println(names[i]);
+        }
+        int j;
         int sum = 0;
-    while(true)
-    {
-        System.out.println("Enter grade or -1");
-        grade = input.nextInt();
-        if(grade == -1)
-            break;
-        grades[count]=grade;
-
-        sum = sum + grades[count];
-        count++;
-    }
-
-        int average = sum/count;
+        for (j = 0; j < counter; j++){
+            sum = sum + grades[j];
+        }
+        int average = sum / counter;
         System.out.println(average);
     }
 }
